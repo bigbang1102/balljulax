f508c1c71747305b079d181e74582a78c29861eb
bigbang1102 <151818283+bigbang1102@users.noreply.github.com>
2024-01-30 16:44:21 +0700

    Update error.js

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,9 +1,4 @@
 const errorHandler = (err, req, res, next) => {
-  // if(err.name ==="Token error") {
-  //     err.statusCode = 400;
-  //     err.message = "Token error"
-  // }
-
   res
     .status(err.statusCode || 500)
     .json({ message: err.message || "Internal Server Error" });
